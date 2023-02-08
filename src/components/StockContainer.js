164504fc9a3cd1cbf14e4ed581167e87c59e9f34
Ziru Zou <jiyucoding@yahoo.com>
2023-02-08 05:19:25 -0500

    take onAddStockToPortfolio as prop, pass it into Stock component

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer( {stocks} ) {
+function StockContainer( {stocks, onAddStockToPortfolio } ) {
 
   return (
     <div>
       <h2>Stocks</h2>
       {/* render stock list here*/}
       {stocks.map(stock => 
-        <Stock key={stock.id} stock={stock}/>
+        <Stock key={stock.id} stock={stock} onAddStockToPortfolio={onAddStockToPortfolio}/>
       )}
     </div>
   );
