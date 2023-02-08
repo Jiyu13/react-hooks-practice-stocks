c54def22f0f0745d331d819de983a72d793fd42e
Ziru Zou <jiyucoding@yahoo.com>
2023-02-08 05:20:40 -0500

    create CB onAddStockToPortfolio, set new state called boughtStocks, update it in CB, pass it to PortfolioContainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,6 +6,7 @@ import SearchBar from "./SearchBar";
 function MainContainer() {
 
   const [stocks, setStocks] = useState([])
+  const [boughtStocks, setBoughtStocks] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:3001/stocks")
@@ -13,15 +14,20 @@ function MainContainer() {
     .then(data => setStocks(data))
   }, [])
 
+  function onAddStockToPortfolio(addedStock) {
+    setBoughtStocks([...boughtStocks, addedStock])
+    console.log(boughtStocks)
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer stocks={stocks}/>
+          <StockContainer stocks={stocks} onAddStockToPortfolio={onAddStockToPortfolio}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer boughtStocks={boughtStocks}/>
         </div>
       </div>
     </div>
