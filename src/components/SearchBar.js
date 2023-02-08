13a67bbb31843042e9bdfadb0a9f15c63b54f27b
Ziru Zou <jiyucoding@yahoo.com>
2023-02-08 07:01:42 -0500

    add onChange event to <select>, create handleFilter, call onFilterChange & pass in selected value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar( {onFilterChange} ) {
+
+  function handleFilter(e) {
+    onFilterChange(e.target.value)
+  }
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -27,7 +32,8 @@ function SearchBar() {
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleFilter}>
+          <option value="Tech">Tech</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
