b50fe1bef41a5940993acd866e3d7ca332262a74
Ziru Zou <jiyucoding@yahoo.com>
2023-02-08 05:21:44 -0500

    take boughtStocks as prop, map it, render a <Stock/> for each bought stock

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer( {boughtStocks} ) {
+
   return (
     <div>
       <h2>My Portfolio</h2>
       {
         //render your portfolio stocks here
+        boughtStocks.map(stock => <Stock key={stock.id} stock={stock}/>)
       }
     </div>
   );
